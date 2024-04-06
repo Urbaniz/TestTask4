370ad8cedcb58482d4cf34abc9452ab54ff2ef19
Urbanovskaia Ekaterina <katya.urban.g@yandex.ru>
2024-04-07 01:43:18 +0300

    Added query "create table Basket"

--- Table/CreateStructure.sql
+++ Table/CreateStructure.sql
@@ -14,3 +14,20 @@ create table Family(
     BudgetValue int not null,
     constraint PK_Family primary key (ID)
 )
+
+-- Создание таблицы Basket
+create table Basket(
+    ID int not null identity,
+    ID_SKU int not null,
+    ID_Family int not null,
+    Quantity int,
+    Value int,
+    PurchaseDate date not null,
+    DiscountValue int,
+    constraint PK_Basket primary key (ID),
+	constraint FK_Basket_ID_SKU_SKU foreign key (ID_SKU) references SKU(ID),
+	constraint FK_Basket_ID_Family_Family foreign key (ID_Family) references Family(ID),
+	constraint CK_Basket_Quantity check(Quantity >= 0),
+	constraint CK_Basket_Value check(Value >= 0)
+)
+alter table Basket add constraint DF_Basket_PurchaseDate default getdate() for PurchaseDate
